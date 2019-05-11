66c7d8d4dca531cf5aabbeffcafc82c8ad174d31
HaQadosch Berraka <haqadosch@pm.me>
2019-05-11 18:05:25 +0100

    First import, bare bone React.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FrontEndMasters_CompleteIntroToReactV5
 https://frontendmasters.com/workshops/complete-intro-react-v5/
+
+https://btholt.github.io/complete-intro-to-react-v5/ 
---
+++ adopt-me/src/index.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return window.React.createElement(
+    'div',
+    {},
+    window.React.createElement(
+      'h1',
+      {},
+      'Adopt Me !'
+    )
+  )
+}
+
+window.ReactDOM.render(window.React.createElement(App), document.getElementById('root'))
